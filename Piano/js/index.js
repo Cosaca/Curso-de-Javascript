91195a61107cee8bcc2136f0ca17893f5b2064d0
cosaca <alegre_samuel@hotmail.com>
2016-03-16 19:01:56 +0000

    añadiendo piano

---
+++ Piano/js/index.js
@@ -0,0 +1,64 @@
+var C1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/c1.mp3')
+  audio.play()
+}
+
+var C1S = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/c1s.mp3')
+  audio.play()
+}
+
+var D1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/d1.mp3')
+  audio.play()
+}
+
+var D1S = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/d1s.mp3')
+  audio.play()
+}
+
+var E1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/e1.mp3')
+  audio.play()
+}
+
+var F1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/f1.mp3')
+  audio.play()
+}
+
+var F1S = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/f1s.mp3')
+  audio.play()
+}
+
+var G1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/g1.mp3')
+  audio.play()
+}
+
+var G1S = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/g1s.mp3')
+  audio.play()
+}
+
+var A1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/a1.mp3')
+  audio.play()
+}
+
+var A1S = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/a1s.mp3')
+  audio.play()
+}
+
+var B1 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/b1.mp3')
+  audio.play()
+}
+
+var C2 = function() {
+  var audio = new Audio('https://raw.githubusercontent.com/pffy/mp3-piano-sound/master/mp3/c2.mp3')
+  audio.play()
+}
